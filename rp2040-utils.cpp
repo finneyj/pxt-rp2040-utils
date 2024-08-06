fec21b69fcef10010b540c6ba4b3f80b28ed0acc
Joe Finney <joe@comp.lancs.ac.uk>
2024-08-06 13:31:50 +0100

    Use internal head definitions

--- rp2040-utils.cpp
+++ rp2040-utils.cpp
@@ -1,13 +1,22 @@
 #include "pxt.h"
-#include "pico/divider.h"
-#include "pico/stdlib.h"
-#include "hardware/vreg.h"
 
+void set_sys_clock_pll(uint32_t vco_freq, uint post_div1, uint post_div2);
+bool check_sys_clock_khz(uint32_t freq_khz, uint *vco_freq_out, uint *post_div1_out, uint *post_div2_out);
 /**
 * share
 * use
 */
 
+static inline bool set_sys_clock_khz(uint32_t freq_khz, bool required) {
+    uint vco, postdiv1, postdiv2;
+    if (check_sys_clock_khz(freq_khz, &vco, &postdiv1, &postdiv2)) {
+        set_sys_clock_pll(vco, postdiv1, postdiv2);
+        return true;
+    } else if (required) {
+        panic("System clock of %u kHz cannot be exactly achieved", freq_khz);
+    }
+    return false;
+}
 
 //% color=#1a3300 weight=99 icon="\uf1e6" block="RP2040 Utils
 namespace rp2040utils {
