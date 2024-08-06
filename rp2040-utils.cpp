560c02c5718030d354ab9b1881ef34bd855d8e36
Joe Finney <joe@comp.lancs.ac.uk>
2024-08-06 13:05:33 +0100

    Introduce underlying system clock functionality

--- rp2040-utils.cpp
+++ rp2040-utils.cpp
@@ -9,21 +9,34 @@
 //% color=#1a3300 weight=99 icon="\uf1e6" block="RP2040 Utils
 namespace rp2040utils {
 
+    int last_frequency = 0;
+
     //%
     int getClockFrequency()
     {
-        return 1;
+        return last_frequency;
     }
 
     //%
     int setClockFrequency(int frequency_khz)
     {
-        return 2;
+        vreg_set_voltage(VREG_VOLTAGE_MAX);
+        if (set_sys_clock_khz(frequency_khz, false))
+        {
+            frequency = frequency_khz;
+            return 1;
+        }
+        
+        return 0;
     }
 
     //%
     int isValidClockFrequency(int frequency_khz)
     {
-        return 3;
+        uint vco, postdiv1, postdiv2;
+        if (check_sys_clock_khz(freq_khz, &vco, &postdiv1, &postdiv2))
+            return 1;
+
+        return 0;
     }
 };
