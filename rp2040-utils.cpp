c1bb9bc7d460e8ecdab5529fc825ec67828a362b
Joe Finney <joe@comp.lancs.ac.uk>
2024-08-06 13:53:43 +0100

    Revert to simple test

--- rp2040-utils.cpp
+++ rp2040-utils.cpp
@@ -1,12 +1,13 @@
 #include "pxt.h"
 
-void set_sys_clock_pll(uint32_t vco_freq, uint post_div1, uint post_div2);
-bool check_sys_clock_khz(uint32_t freq_khz, uint *vco_freq_out, uint *post_div1_out, uint *post_div2_out);
+//void set_sys_clock_pll(uint32_t vco_freq, uint post_div1, uint post_div2);
+//bool check_sys_clock_khz(uint32_t freq_khz, uint *vco_freq_out, uint *post_div1_out, uint *post_div2_out);
 /**
 * share
 * use
 */
 
+/*
 static inline bool set_sys_clock_khz(uint32_t freq_khz, bool required) {
     uint vco, postdiv1, postdiv2;
     if (check_sys_clock_khz(freq_khz, &vco, &postdiv1, &postdiv2)) {
@@ -17,38 +18,41 @@ static inline bool set_sys_clock_khz(uint32_t freq_khz, bool required) {
     }
     return false;
 }
+*/
 
 //% color=#1a3300 weight=99 icon="\uf1e6" block="RP2040 Utils
 namespace rp2040utils {
 
-    int last_frequency = 0;
+    //int last_frequency = 0;
 
     //%
     int getClockFrequency()
     {
-        return last_frequency;
+        return 10;
     }
 
     //%
     int setClockFrequency(int frequency_khz)
     {
+        /*
         vreg_set_voltage(VREG_VOLTAGE_MAX);
         if (set_sys_clock_khz(frequency_khz, false))
         {
             frequency = frequency_khz;
             return 1;
         }
-        
-        return 0;
+        */
+        return 11;
     }
 
     //%
     int isValidClockFrequency(int frequency_khz)
     {
+        /*
         uint vco, postdiv1, postdiv2;
         if (check_sys_clock_khz(freq_khz, &vco, &postdiv1, &postdiv2))
             return 1;
-
-        return 0;
+        */
+        return 12;
     }
 };
