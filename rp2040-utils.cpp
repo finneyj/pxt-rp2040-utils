d7d345b206a92b6468c03e17fd41bb68bd9b2fa0
Joe Finney <joe@comp.lancs.ac.uk>
2024-08-07 16:49:17 +0100

    Reintroduce clock code

--- rp2040-utils.cpp
+++ rp2040-utils.cpp
@@ -7,52 +7,47 @@
 * use
 */
 
-/*
-static inline bool set_sys_clock_khz(uint32_t freq_khz, bool required) {
+static inline bool set_sys_clock_khz(uint32_t freq_khz) {
     uint vco, postdiv1, postdiv2;
     if (check_sys_clock_khz(freq_khz, &vco, &postdiv1, &postdiv2)) {
         set_sys_clock_pll(vco, postdiv1, postdiv2);
         return true;
-    } else if (required) {
-        panic("System clock of %u kHz cannot be exactly achieved", freq_khz);
     }
+
     return false;
 }
-*/
 
 //% color=#1a3300 weight=99 icon="\uf1e6" block="RP2040 Utils
 namespace rp2040utils {
 
-    //int last_frequency = 0;
+    int last_frequency = 0;
 
     //%
     int getClockFrequency()
     {
-        return 10;
+        return last_frequency;
     }
 
     //%
     int setClockFrequency(int frequency_khz)
     {
-        /*
         vreg_set_voltage(VREG_VOLTAGE_MAX);
         if (set_sys_clock_khz(frequency_khz, false))
         {
             frequency = frequency_khz;
             return 1;
         }
-        */
-        return 11;
+        
+        return 0;
     }
 
     //%
     int isValidClockFrequency(int frequency_khz)
     {
-        /*
         uint vco, postdiv1, postdiv2;
         if (check_sys_clock_khz(freq_khz, &vco, &postdiv1, &postdiv2))
             return 1;
-        */
-        return 12;
+        
+        return 0;
     }
 };
